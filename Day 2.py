f6d5705bdc82dbc20ac1594ce26cc847bf2405c9
misbahmustafam-collab <misbahmustafam@gmail.com>
2026-07-27 14:36:51 +0500

    Create Day 2.py
    Day 2: Completed Python Basics

---
+++ Day 2.py
@@ -0,0 +1,52 @@
+Name="Misbah"
+Age="20"
+print(Name)
+print(Age)
+
+#data type
+Name="Misbah"
+Age="20"
+Marks="30"
+student_of="Data sceince"
+
+
+print(Name)
+print(Age)
+print(Marks)
+print(student_of)
+
+#operators
+a=14
+b=10
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+
+#loops
+for i in range(3,5):
+    print(i)
+
+    #while loop
+    count=3
+    while count<=5:
+        print(count)
+        count+=3
+
+
+#functions
+def greet():
+    print("Hello from function")
+
+greet()
+
+
+#2....
+def add_numbers(x,y):
+    return(x-y)
+results=add_numbers(8,4)
+print(results)
+
+
